8af2ac91b9b97f371cd508564fdc883c976c43d2
Harshit <devinnow8@gmail.com>
2023-03-30 11:54:02 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -15,6 +15,7 @@ const handleResponse = async (conCatString) => {
     frequency_penalty: 0,
     presence_penalty: 0,
   });
+  console.log(response, "responseresponse==>>1234");
   return response.data.choices[0].text.split(":")[1];
 };
 
