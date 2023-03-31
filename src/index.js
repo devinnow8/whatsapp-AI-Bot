cdb0bfd9fa627b100365acaaf011814ae0e440b1
Harshit <devinnow8@gmail.com>
2023-03-31 12:53:56 +0530

    response url added

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ const findUser = (user) => {
           let conCatString = messagesArr.join("\\n") + "\\n";
           const response = await handleResponse(conCatString);
           messagesArr.splice(messagesArr.length - 1, 1, messagesArr[messagesArr.length - 1] + `\nAI:${response}`);
-          await bot.sendText(msg.from, response);
+          await bot.sendText(msg.from, response, { preview_url: true });
         } catch (error) {
           console.error(error, "error===>");
         }
