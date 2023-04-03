c14f35d5122c50ea0857ea3a0c68c95dc7579844
Harshit <devinnow8@gmail.com>
2023-04-03 14:44:43 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,19 +1,5 @@
 const { Configuration, OpenAIApi } = require("openai");
 
-function removeMultipleColons(str) {
-  // Check if the string contains more than 2 consecutive colons
-  const regex = /:{3,}/g;
-  if (regex.test(str)) {
-    // Replace all occurrences of more than 2 consecutive colons with a single colon
-    str = str.replace(regex, ":");
-    // Call the function recursively to check for additional occurrences
-    return removeMultipleColons(str);
-  } else {
-    // If no additional occurrences are found, return the modified string
-    return str;
-  }
-}
-
 const handleResponse = async (conCatString) => {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -30,7 +16,7 @@ const handleResponse = async (conCatString) => {
     presence_penalty: 0,
   });
   console.log("response====>", response.data.choices[0]);
-  const formattedResponse = removeMultipleColons(response.data.choices[0].text);
+  const formattedResponse = response.data.choices[0].text.replace(/:{2,}/g, ":");
   return formattedResponse.slice(3).trim();
 };
 
