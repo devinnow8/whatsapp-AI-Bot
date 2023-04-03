45c4723a48a662c611606d6c25b9aa84e9670404
Harshit <devinnow8@gmail.com>
2023-04-03 16:01:44 +0530

    updated changes

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,121 +1,71 @@
 const chatHistory = require("../models/history");
 const telegramChatHistory = require("../models/telegram-chat");
 
-const getResponseData = async (req, res, next) => {
+// Common function to get response data from the database
+const getResponseData = async (model, userNumber) => {
   try {
-    const isResponse = await chatHistory.findOne({
-      userNumber: req,
-    });
-    if (!isResponse) {
-      return false;
-    }
-    return isResponse;
+    const response = await model.findOne({ userNumber });
+    return response || false;
   } catch (error) {
-    return error.message;
+    throw new Error(error.message);
   }
 };
 
-const saveResponseData = async (msg, array) => {
+// Common function to save response data to the database
+const saveResponseData = async (model, userNumber, userName, text) => {
   try {
-    let userExist = await getResponseData(msg.from);
+    const userExist = await getResponseData(model, userNumber);
     console.log(userExist, "userExist==>>>1234");
     if (!userExist) {
-      let newResponseTable = new chatHistory({
-        userNumber: msg.from,
-        userName: msg.name,
-        text: [`${msg.name}: ${msg.data.text}\nAI:\nHello ${msg.name}`],
+      const newResponseTable = new model({
+        userNumber,
+        userName,
+        text,
       });
-      newResponseTable = newResponseTable.save();
-      if (!newResponseTable) {
-        return "not able to save";
-      }
-      return newResponseTable;
+      const savedResponse = await newResponseTable.save();
+      return savedResponse;
     } else {
       const now = new Date();
-      const messageTimestamp = new Date(userExist?.updatedAt); // Replace with actual message timestamp
-      // const hoursDiff = Math.abs(now - messageTimestamp) / 36e5;
-
+      const messageTimestamp = new Date(userExist?.updatedAt);
       const hoursDiff = Math.abs(now - messageTimestamp) / 6e4;
-
       console.log(hoursDiff, "hoursDiff==>>>>112");
-
-      if (hoursDiff >= 2) {
-        console.log("Message is 1 hour or older");
-        userExist = await chatHistory.findOneAndUpdate({ userNumber: msg.from }, { text: array[array.length - 1] }, { new: true });
-      } else {
-        console.log("Message is less than 1 hour old");
-        userExist = await chatHistory.findOneAndUpdate({ userNumber: msg.from }, { text: array }, { new: true });
-      }
-
-      // userExist = await chatHistory.findOneAndUpdate({ userNumber: msg.from }, { text: array }, { new: true });
-      console.log(userExist, "userExist++====>12345");
-      return userExist;
+      const updatedResponse =
+        hoursDiff >= 2 ? await model.findOneAndUpdate({ userNumber }, { text: text[text.length - 1] }, { new: true }) : await model.findOneAndUpdate({ userNumber }, { text }, { new: true });
+      console.log(updatedResponse, "updatedResponse++====>12345");
+      return updatedResponse;
     }
   } catch (error) {
-    return error.message;
+    throw new Error(error.message);
   }
 };
 
-const telegramGetResponseData = async (req) => {
-  try {
-    const isResponse = await telegramChatHistory.findOne({
-      userNumber: req,
-    });
-    if (!isResponse) {
-      return false;
-    }
-    return isResponse;
-  } catch (error) {
-    return error.message;
-  }
+// Get response data for regular chat
+const getChatResponseData = async (userNumber) => {
+  return await getResponseData(chatHistory, userNumber);
 };
 
-const telegramSaveResponseData = async (msg, array) => {
-  const chatId = msg.chat.id;
-  let name = msg.from.first_name;
-  try {
-    let userExist = await telegramGetResponseData(chatId);
-    console.log(userExist, "userExist==>>>1234");
-    if (!userExist) {
-      let newResponseTable = new telegramChatHistory({
-        userNumber: chatId,
-        userName: name,
-        text: [`${name}: ${msg.text}\nAI:\nHello ${name}`],
-      });
-      newResponseTable = newResponseTable.save();
-      if (!newResponseTable) {
-        return "not able to save";
-      }
-      return newResponseTable;
-    } else {
-      const now = new Date();
-      const messageTimestamp = new Date(userExist?.updatedAt); // Replace with actual message timestamp
-      // const hoursDiff = Math.abs(now - messageTimestamp) / 36e5;
+// Save response data for regular chat
+const saveChatResponseData = async (msg, text) => {
+  const userNumber = msg.from;
+  const userName = msg.name;
+  return await saveResponseData(chatHistory, userNumber, userName, text);
+};
 
-      const hoursDiff = Math.abs(now - messageTimestamp) / 6e4;
+// Get response data for Telegram chat
+const getTelegramResponseData = async (userNumber) => {
+  return await getResponseData(telegramChatHistory, userNumber);
+};
 
-      console.log(hoursDiff, "hoursDiff==>>>>112");
-
-      if (hoursDiff >= 2) {
-        console.log("Message is 1 hour or older");
-        userExist = await telegramChatHistory.findOneAndUpdate({ userNumber: chatId }, { text: array[array.length - 1] }, { new: true });
-      } else {
-        console.log("Message is less than 1 hour old");
-        userExist = await telegramChatHistory.findOneAndUpdate({ userNumber: chatId }, { text: array }, { new: true });
-      }
-
-      // userExist = await chatHistory.findOneAndUpdate({ userNumber: msg.from }, { text: array }, { new: true });
-      console.log(userExist, "userExist++====>12345");
-      return userExist;
-    }
-  } catch (error) {
-    return error.message;
-  }
+// Save response data for Telegram chat
+const saveTelegramResponseData = async (msg, text) => {
+  const userNumber = msg.chat.id;
+  const userName = msg.from.first_name;
+  return await saveResponseData(telegramChatHistory, userNumber, userName, text);
 };
 
 module.exports = {
-  getResponseData,
-  saveResponseData,
-  telegramSaveResponseData,
-  telegramGetResponseData,
+  getChatResponseData,
+  saveChatResponseData,
+  getTelegramResponseData,
+  saveTelegramResponseData,
 };
--- src/index.js
+++ src/index.js
@@ -1,98 +1,110 @@
+// Import required modules
 require("dotenv").config();
-const { saveResponseData, getResponseData, telegramGetResponseData, telegramSaveResponseData } = require("./controllers/api");
 const { dbConnect } = require("./DB/dbConnect");
 const { createBot } = require("whatsapp-cloud-api");
-const { handleResponse } = require("./bot");
 const TelegramBot = require("node-telegram-bot-api");
+const { saveChatResponseData, getChatResponseData, getTelegramResponseData, saveTelegramResponseData } = require("./controllers/api");
+const { handleResponse } = require("./bot");
+
+// Set up Telegram bot
 const token = process.env.TELEGRAM_TOKEN;
 const telegramBot = new TelegramBot(token, { polling: true });
 
+// Define common function to handle incoming messages
+const handleMessage = async (msg, isTelegram) => {
+  console.log(msg);
+
+  // Check if user exists in database
+  let userExist = isTelegram ? await getTelegramResponseData(msg.chat.id) : await getChatResponseData(msg.from);
+  let messagesArr;
+
+  if (userExist) {
+    try {
+      messagesArr = userExist.text;
+
+      // Limit message history to 5 messages
+      if (messagesArr.length > 5) {
+        messagesArr.splice(0, 1);
+      }
+
+      // Add new message to message history
+      messagesArr.push(`${isTelegram ? msg.from.first_name : msg.name}:${isTelegram ? msg.text : msg.data.text}`);
+
+      // Concatenate message history into a single string and pass to AI for processing
+      let conCatString = messagesArr.join("\\n") + "\\n";
+      const response = await handleResponse(conCatString);
+      console.log(response, "response==>check");
+
+      // Update last message in message history with AI response
+      messagesArr.splice(messagesArr.length - 1, 1, messagesArr[messagesArr.length - 1] + `\nAI:\n${response}`);
+
+      // Send AI response to user
+      if (isTelegram) {
+        await telegramBot.sendMessage(msg.chat.id, response);
+      } else {
+        await bot.sendText(msg.from, response, { preview_url: true });
+      }
+    } catch (error) {
+      console.error(error, "error===>");
+
+      // Send error message to user
+      if (isTelegram) {
+        await telegramBot.sendMessage(msg.chat.id, `Sorry we can't proceed your request`);
+      } else {
+        await bot.sendText(msg.from, `Sorry we can't proceed your request`);
+      }
+    }
+  } else {
+    try {
+      // Send welcome message to new user
+      if (isTelegram) {
+        await telegramBot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}`);
+        saveTelegramResponseData(msg);
+      } else {
+        await bot.sendText(msg.from, `Hello ${msg.name}`);
+        saveChatResponseData(msg);
+      }
+    } catch (error) {
+      console.error(error, "error===>");
+    }
+  }
+
+  // Save updated message history to database
+  if (isTelegram) {
+    saveTelegramResponseData(msg, messagesArr);
+  } else {
+    saveChatResponseData(msg, messagesArr);
+  }
+};
+
+// Set up WhatsApp bot
 (async () => {
   await dbConnect();
-  try {
-    // replace the values below from the values you copied above
-    const from = process.env.FROM,
-      token = process.env.TOKEN,
-      to = process.env.TO, // your phone number without the leading '+'
-      webhookVerifyToken = process.env.WEBHOOKVERIFYTOKEN, // use a random value, e.g. 'bju#hfre@iu!e87328eiekjnfw'
-      bot = createBot(from, token),
-      // Start express server to listen for incoming messages
-      { app } = await bot.startExpressServer({
-        webhookVerifyToken,
-      });
 
+  try {
+    const from = process.env.FROM;
+    const token = process.env.TOKEN;
+    const to = process.env.TO;
+    const webhookVerifyToken = process.env.WEBHOOKVERIFYTOKEN;
+    const bot = createBot(from, token);
+
+    const { app } = await bot.startExpressServer({
+      webhookVerifyToken,
+    });
+
+    // Set up privacy policy route
     app.get("/privacy-policy", (req, res) => {
       res.sendFile(__dirname + "/privacy-policy.html");
     });
 
-    // Listen to ALL incoming whatsapp messages
+    // Listen to incoming WhatsApp messages
     bot.on("message", async (msg) => {
-      console.log(msg);
-      let userExist = await getResponseData(msg.from);
-      let messagesArr;
-      if (userExist) {
-        try {
-          messagesArr = userExist.text;
-          if (messagesArr.length > 5) {
-            messagesArr.splice(0, 1);
-          }
-          messagesArr.push(`${msg.name}:${msg.data.text}`);
-
-          let conCatString = messagesArr.join("\\n") + "\\n";
-          const response = await handleResponse(conCatString);
-          console.log(response, "response==>check");
-          messagesArr.splice(messagesArr.length - 1, 1, messagesArr[messagesArr.length - 1] + `\nAI:\n${response}`);
-          await bot.sendText(msg.from, response, { preview_url: true });
-        } catch (error) {
-          console.error(error, "error===>");
-          await bot.sendText(msg.from, `Sorry we can't proceed your request`);
-        }
-      } else {
-        try {
-          await bot.sendText(msg.from, `Hello ${msg.name}`);
-          saveResponseData(msg);
-        } catch (error) {
-          console.error(error, "error===>");
-        }
-      }
-      saveResponseData(msg, messagesArr);
+      await handleMessage(msg, false);
     });
 
-    // Listen to ALL incoming telegram messages
+    // Listen to incoming Telegram messages
     telegramBot.on("message", async (msg) => {
-      const chatId = String(msg.chat.id);
-      console.log(msg, "msgmsgmsg", typeof chatId);
-      // telegramBot.sendMessage(chatId, "Received your message: " + msg.text);
-
-      let userExist = await telegramGetResponseData(chatId);
-      let name = msg.from.first_name;
-      let messagesArr;
-      if (userExist) {
-        try {
-          messagesArr = userExist.text;
-          if (messagesArr.length > 5) {
-            messagesArr.splice(0, 1);
-          }
-          messagesArr.push(`${name}:${msg.text}`);
-
-          let conCatString = messagesArr.join("\\n") + "\\n";
-          const response = await handleResponse(conCatString);
-          console.log(response, "response==>check");
-          messagesArr.splice(messagesArr.length - 1, 1, messagesArr[messagesArr.length - 1] + `\nAI:\n${response}`);
-          await telegramBot.sendMessage(chatId, response);
-        } catch (error) {
-          console.error(error, "error===>");
-          await telegramBot.sendMessage(chatId, `Sorry we can't proceed your request`);
-        }
-      } else {
-        try {
-          await telegramBot.sendMessage(chatId, `Hello ${name}`);
-          telegramSaveResponseData(msg);
-        } catch (error) {
-          console.error(error, "error===>");
-        }
-      }
-      telegramSaveResponseData(msg, messagesArr);
+      await handleMessage(msg, true);
     });
   } catch (err) {
     console.error(err);
